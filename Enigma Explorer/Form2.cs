22b3291bd5c80edb2d1dc55117fa41e5ed10e9aa
manimekala123 <139643976+manimekala123@users.noreply.github.com>
2023-12-25 16:09:34 +0530

    Update Form2.cs

--- Enigma Explorer/Form2.cs
+++ Enigma Explorer/Form2.cs
@@ -21,5 +21,67 @@ namespace Enigma_Explorer
         {
 
         }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+                Color select_color = Color.FromArgb(46, 49, 49);
+        private void button_GoToLogin_Click(object sender, EventArgs e)
+        {
+            panel_login.BringToFront();
+            button_GoToLogin.BackColor = select_color;
+            panel_login.BackColor = select_color;
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            string enteredUsername = username.Text.Trim();
+            string enteredPassword = Password.Text.Trim();
+
+            if (string.IsNullOrEmpty(enteredUsername) || string.IsNullOrEmpty(enteredPassword))
+            {
+                MessageBox.Show("Please enter both username and password.");
+                return;
+            }
+
+            //Read the file and check for the user credentials
+            string[] lines = File.ReadAllLines(FilePath);
+             foreach (string line in lines)
+             {
+                 string[] parts = line.Split(',');
+                 string savedUsername = parts[0].Split(':')[1].Trim();
+                 string savedPassword = parts[1].Split(':')[1].Trim();
+
+                 if (savedUsername == enteredUsername && savedPassword == enteredPassword)
+                 {
+                    MessageBox.Show("Login successful!");
+                    Form4 form4 = new Form4();
+
+                    // Show Form1 and hide Form2
+                    form4.Show();
+                    this.Hide();
+                     ClearFields();
+                     return;
+                 }
+             }
+
+             MessageBox.Show("Invalid username or password.");
+         }
+
+         private void ClearFields()
+         {
+             username.Clear();
+             Password.Clear();
+             //clear data
+         }
+
+
+      private void button_GoToRegister_Click(object sender, EventArgs e)
+     {
+        Form3 form3 = new Form3();
+         form3.Show(); 
+         this.Hide();
+     }       
     }
+}
 }
