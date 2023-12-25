67c7312fc61f3f63416d97d542afc8e574bf6563
Udara Nalawansa <udarakavishka13@gmail.com>
2023-12-25 10:57:48 +0530

    Movie Form Completed

--- Enigma Explorer/Form4.Designer.cs
+++ Enigma Explorer/Form4.Designer.cs
@@ -37,7 +37,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonMovie = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -55,9 +55,10 @@
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Baskerville Old Face", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.Lavender;
-            this.label7.Location = new System.Drawing.Point(273, 69);
+            this.label7.Location = new System.Drawing.Point(205, 56);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(272, 23);
+            this.label7.Size = new System.Drawing.Size(224, 18);
             this.label7.TabIndex = 33;
             this.label7.Text = "Journey Into Student Hobbies";
             // 
@@ -65,11 +66,12 @@
             // 
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("STCaiyun", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label6.Location = new System.Drawing.Point(164, -3);
+            this.label6.Location = new System.Drawing.Point(123, -2);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(485, 63);
+            this.label6.Size = new System.Drawing.Size(408, 55);
             this.label6.TabIndex = 32;
             this.label6.Text = "Enigma  Explorer";
             // 
@@ -80,9 +82,10 @@
             this.label5.Font = new System.Drawing.Font("Poor Richard", 10.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(157, 524);
+            this.label5.Location = new System.Drawing.Point(118, 426);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(363, 42);
+            this.label5.Size = new System.Drawing.Size(310, 36);
             this.label5.TabIndex = 31;
             this.label5.Text = "Join us in discovering new places and cultures,\r\nOne adventure at a time.\r\n";
             // 
@@ -92,9 +95,10 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Poor Richard", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(231, 569);
+            this.label4.Location = new System.Drawing.Point(173, 462);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 21);
+            this.label4.Size = new System.Drawing.Size(0, 18);
             this.label4.TabIndex = 30;
             // 
             // label3
@@ -104,9 +108,10 @@
             this.label3.Font = new System.Drawing.Font("Poor Richard", 10.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(157, 414);
+            this.label3.Location = new System.Drawing.Point(118, 336);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(385, 42);
+            this.label3.Size = new System.Drawing.Size(325, 36);
             this.label3.TabIndex = 29;
             this.label3.Text = "Form page to page ,embark literary journeys that\r\n camptive the mind.";
             // 
@@ -117,9 +122,10 @@
             this.label2.Font = new System.Drawing.Font("Poor Richard", 10.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(157, 300);
+            this.label2.Location = new System.Drawing.Point(118, 244);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(302, 21);
+            this.label2.Size = new System.Drawing.Size(257, 18);
             this.label2.TabIndex = 28;
             this.label2.Text = "Dive into cinematic stories through us.";
             // 
@@ -130,9 +136,10 @@
             this.label1.Font = new System.Drawing.Font("Poor Richard", 10.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
                 | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(157, 178);
+            this.label1.Location = new System.Drawing.Point(118, 145);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(373, 21);
+            this.label1.Size = new System.Drawing.Size(315, 18);
             this.label1.TabIndex = 27;
             this.label1.Text = "Dive into the world of singing on our homepage";
             // 
@@ -142,9 +149,10 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.RosyBrown;
-            this.button4.Location = new System.Drawing.Point(43, 480);
+            this.button4.Location = new System.Drawing.Point(32, 390);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(168, 39);
+            this.button4.Size = new System.Drawing.Size(126, 32);
             this.button4.TabIndex = 26;
             this.button4.Text = "TRAVELLING PLACES";
             this.button4.UseVisualStyleBackColor = false;
@@ -156,27 +164,29 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.RosyBrown;
-            this.button3.Location = new System.Drawing.Point(43, 358);
+            this.button3.Location = new System.Drawing.Point(32, 291);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(168, 39);
+            this.button3.Size = new System.Drawing.Size(126, 32);
             this.button3.TabIndex = 25;
             this.button3.Text = "BOOKS";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button2
+            // buttonMovie
             // 
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.RosyBrown;
-            this.button2.Location = new System.Drawing.Point(43, 240);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(168, 39);
-            this.button2.TabIndex = 24;
-            this.button2.Text = "MOVIES";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.buttonMovie.BackColor = System.Drawing.Color.Transparent;
+            this.buttonMovie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMovie.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMovie.ForeColor = System.Drawing.Color.RosyBrown;
+            this.buttonMovie.Location = new System.Drawing.Point(32, 195);
+            this.buttonMovie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonMovie.Name = "buttonMovie";
+            this.buttonMovie.Size = new System.Drawing.Size(126, 32);
+            this.buttonMovie.TabIndex = 24;
+            this.buttonMovie.Text = "MOVIES";
+            this.buttonMovie.UseVisualStyleBackColor = false;
+            this.buttonMovie.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -184,9 +194,10 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Algerian", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.RosyBrown;
-            this.button1.Location = new System.Drawing.Point(54, 120);
+            this.button1.Location = new System.Drawing.Point(40, 98);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(168, 39);
+            this.button1.Size = new System.Drawing.Size(126, 32);
             this.button1.TabIndex = 23;
             this.button1.Text = "SONGS";
             this.button1.UseVisualStyleBackColor = false;
@@ -196,9 +207,10 @@
             // 
             this.pictureBox4.BackgroundImage = global::Enigma_Explorer.Properties.Resources.images;
             this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox4.Location = new System.Drawing.Point(661, 348);
+            this.pictureBox4.Location = new System.Drawing.Point(496, 283);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(151, 87);
+            this.pictureBox4.Size = new System.Drawing.Size(113, 71);
             this.pictureBox4.TabIndex = 22;
             this.pictureBox4.TabStop = false;
             // 
@@ -206,9 +218,10 @@
             // 
             this.pictureBox3.BackgroundImage = global::Enigma_Explorer.Properties.Resources._1000_F_265334795_0pP1EJ4NxaW3hBFbNnZebMqSBSZIUUWL1;
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(661, 480);
+            this.pictureBox3.Location = new System.Drawing.Point(496, 390);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(151, 86);
+            this.pictureBox3.Size = new System.Drawing.Size(113, 70);
             this.pictureBox3.TabIndex = 21;
             this.pictureBox3.TabStop = false;
             // 
@@ -216,9 +229,10 @@
             // 
             this.pictureBox2.BackgroundImage = global::Enigma_Explorer.Properties.Resources._1;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(661, 230);
+            this.pictureBox2.Location = new System.Drawing.Point(496, 187);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(151, 91);
+            this.pictureBox2.Size = new System.Drawing.Size(113, 74);
             this.pictureBox2.TabIndex = 20;
             this.pictureBox2.TabStop = false;
             // 
@@ -226,19 +240,20 @@
             // 
             this.pictureBox1.BackgroundImage = global::Enigma_Explorer.Properties.Resources.a_logo_for_a_music_company_that_is_made_by_song_brand_vector;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(661, 120);
+            this.pictureBox1.Location = new System.Drawing.Point(496, 98);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(151, 92);
+            this.pictureBox1.Size = new System.Drawing.Size(113, 75);
             this.pictureBox1.TabIndex = 19;
             this.pictureBox1.TabStop = false;
             // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Enigma_Explorer.Properties.Resources.Background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(855, 586);
+            this.ClientSize = new System.Drawing.Size(641, 476);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -248,12 +263,13 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonMovie);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Home";
             this.Text = "Form4";
             this.Load += new System.EventHandler(this.Home_Load);
@@ -277,7 +293,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonMovie;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox3;
